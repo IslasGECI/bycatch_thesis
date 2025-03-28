d0e9d2dba59bf829c85f84cc1f2d0a227dfcf410
Mario Villasante <mario.villasante@islas.org.mx>
2025-03-28 10:38:25 -0700

    🏃🏽‍♂️ 🚀 scrip to clean fisherie data

---
+++ src/concatenate_fishery_data.R
@@ -0,0 +1,9 @@
+library(fs)
+library(purrr)
+library(tidyverse)
+
+
+csv_file_list <- dir_ls("/workdir/data/raw/RLMSEP_2017/", recurse = T, regexp = "[.]csv$")
+df_list <- map(csv_file_list, read_csv, na = c("", "NULL"), col_types = list(FechaRecepcionUnitrac = col_datetime(format = "%d/%m/%Y %H:%M")))
+bind_rows(df_list) |>
+  write_csv("/workdir/data/processed/fisheries_gps_points_ascii.csv")
