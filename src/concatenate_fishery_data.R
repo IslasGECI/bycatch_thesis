984944186d99d3bdd750216d942dd764387aba51
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2026-03-04 11:35:18 -0800

    🗄  Ensure consistent column parsing for CONAPESCA CSV ingestion

--- src/concatenate_fishery_data.R
+++ src/concatenate_fishery_data.R
@@ -3,7 +3,18 @@ library(purrr)
 library(tidyverse)
 
 
-csv_file_list <- dir_ls("/workdir/data/raw/conapesca/", recurse = T, regexp = "[.]csv$")
-df_list <- map(csv_file_list, read_csv, na = c("", "NULL"), col_types = list(FechaRecepcionUnitrac = col_datetime(format = "%d/%m/%Y %H:%M")))
+csv_file_list <- dir_ls("/workdir/data/raw/conapesca/", recurse = TRUE, regexp = "[.]csv$")
+df_list <- map(
+  csv_file_list,
+  read_csv,
+  na = c("", "NULL"),
+  col_types = cols(
+    FechaRecepcionUnitrac = col_datetime(format = "%d/%m/%Y %H:%M"),
+    RNP = col_character(),
+    RNPA = col_character(),
+    Rumbo = col_character(),
+    Velocidad = col_character()
+  )
+)
 bind_rows(df_list) |>
   write_csv("/workdir/data/processed/fisheries_gps_points_ascii.csv")
